956ebac7cfacdaaac34d5f23eae6a2864923030d
Robson Ruiz <robson@portalqualis.com.br>
2021-02-23 10:09:39 -0300

    retira alterações

--- src/index.js
+++ src/index.js
@@ -4,26 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const http = require('http');
-
-const hostname = '0.0.0.0';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.end(
-    ReactDOM.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-      document.getElementById('root')
-    )
-  );
-})
-
-server.listen(port, hostname, () => {
-  console.log('Server running at http://${hostname}:${port}/');
-})
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
